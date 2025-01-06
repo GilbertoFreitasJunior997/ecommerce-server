bd6f870332070a8d01869fa295b228fb5cd5c8df
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2025-01-06 06:18:48 -0300

    fix: cors url issue

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,11 @@ import { createContext } from "./trpc/context";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  }),
+);
 
 app.use(
   "/trpc",
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
-  greeting: publicProcedure.query(() => process.env.CLIENT_URL),
+  greeting: publicProcedure.query(() => "Hello world"),
 });
 
 export type AppRouter = typeof appRouter;
